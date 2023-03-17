72b16953e9163a53f9823f45530c3daa7eed86e6
diego-domajnko <diegodomajnko@gmail.com>
2023-03-17 09:47:47 -0300

    update jogadas maximas

--- assets/js/main.js
+++ assets/js/main.js
@@ -176,6 +176,7 @@ let letras = [
     linha: '6'
   }
 ]
+let contJogadas = 0;
 const btnTeclado = document.querySelectorAll('.letra');
 const palavra = escolherPalavraAleatoria().toUpperCase();
 const msgErro = document.querySelector('.msg');
@@ -189,21 +190,24 @@ function escolherPalavraAleatoria() {
 }
 
 btnTeclado.forEach(e => {
-  e.addEventListener('click', event => {
-    if (!msgErro.classList.contains('acerto')) {
-      const letraClicada = event.target.textContent;
-      if (letraClicada == 'Enter') {
-        testarPalavra();
-      } else if (letraClicada == 'backspace') {
-        apagarLetra();
-      } else {
-        inserirLetra(event.target.textContent);
+  if (contJogadas < 6) {
+    e.addEventListener('click', event => {
+      if (!msgErro.classList.contains('acerto')) {
+        const letraClicada = event.target.textContent;
+        if (letraClicada == 'Enter') {
+          testarPalavra();
+        } else if (letraClicada == 'backspace') {
+          apagarLetra();
+        } else {
+          inserirLetra(event.target.textContent);
+        }
       }
-    }
-  });
+    });
+  }
 });
 
 window.addEventListener('keyup', (e) => {
+  if (contJogadas < 6) {
     let letra = e.key;
     if (!msgErro.classList.contains('acerto')) {
       if (e.keyCode >= 65 && e.keyCode <= 90) {
@@ -214,89 +218,94 @@ window.addEventListener('keyup', (e) => {
         apagarLetra();
       }
     }
+  }
 });
 
 function apagarLetra() {
-    limparAviso();
-    const linha = encontrarLinhaNaoPreenchida();
-    const coluna = encontrarColunaNaoPreenchida();
-    const letraDeletada = letras[linha].colunas[coluna - 1];
-    let posicao = encontrarUltimaPosicaoPreenchida();
+  limparAviso();
+  const linha = encontrarLinhaNaoPreenchida();
+  const coluna = encontrarColunaNaoPreenchida();
+  const letraDeletada = letras[linha].colunas[coluna - 1];
+  let posicao = encontrarUltimaPosicaoPreenchida();
 
-    if (letras[linha].colunas[0] != false) {
-      if (!posicao.classList.contains('linha__wordle')) {
-        posicao = posicao.parentNode.querySelector(`.letra${coluna}`);
-      } else {
-        posicao = posicao.querySelector(`.letra${coluna}`);
-      }
-      
-      if (letraDeletada != false) {
-        posicao.textContent = "";
-        letras[linha].colunas[coluna - 1] = false;
-      }
+  if (letras[linha].colunas[0] != false) {
+    if (!posicao.classList.contains('linha__wordle')) {
+      posicao = posicao.parentNode.querySelector(`.letra${coluna}`);
+    } else {
+      posicao = posicao.querySelector(`.letra${coluna}`);
     }
+    
+    if (letraDeletada != false) {
+      posicao.textContent = "";
+      letras[linha].colunas[coluna - 1] = false;
+    }
+  }
 }
 
 function testarPalavra() {
-    limparAviso();
-    const numLinha = encontrarLinhaNaoPreenchida();
-    const colunas = letras[numLinha].colunas;
-    let todosEspacosPreenchidos = true;
-    let palavraEscrita = "";
-    
-    for (let i = 0; i < colunas.length; i++) {
-      const element = colunas[i];
-      palavraEscrita += element;
-      if (!element) {
-        todosEspacosPreenchidos = false;
-      }
+  limparAviso();
+  const numLinha = encontrarLinhaNaoPreenchida();
+  const colunas = letras[numLinha].colunas;
+  let todosEspacosPreenchidos = true;
+  let palavraEscrita = "";
+  
+  for (let i = 0; i < colunas.length; i++) {
+    const element = colunas[i];
+    palavraEscrita += element;
+    if (!element) {
+      todosEspacosPreenchidos = false;
     }
-    
-    const buscarPalavra = palavras.find(palavra => palavra.palavra.toUpperCase() == palavraEscrita);
-    const palavraExiste = buscarPalavra ? true : false;
-    
-    if (todosEspacosPreenchidos && palavraExiste) {
-      letras[numLinha].linhaPreenchida = true;
-      if (palavraEscrita != palavra) {
-        for (let i = 0; i < palavraEscrita.length; i++) {
-          if (palavraEscrita[i] == palavra[i]) {
-            document.querySelector(`.letra${palavraEscrita[i]}`).style.backgroundColor = "#538D4E";
-            document.querySelector(`.linha__wordle-${encontrarLinhaNaoPreenchida()}`).querySelector(`.letra${i + 1}`).style.backgroundColor = "#538D4E";
-          } else if (palavraEscrita[i] != palavra[i] && palavra.includes(palavraEscrita[i])) {
-            document.querySelector(`.letra${palavraEscrita[i]}`).style.backgroundColor = "#B59F3B";
-            document.querySelector(`.linha__wordle-${encontrarLinhaNaoPreenchida()}`).querySelector(`.letra${i + 1}`).style.backgroundColor = "#B59F3B";
-          } else {
-            document.querySelector(`.letra${palavraEscrita[i]}`).style.backgroundColor = "#585858";
-          }
-        }
-        if (encontrarLinhaNaoPreenchida() == 6) {
-          msgErro.classList.add('fim-de-jogo');
-          msgErro.style.display = "block";
-          msgErro.innerHTML = `Você perdeu o jogo!`;
-        }
-      } else {
-        document.querySelector('.principal').innerHTML += `
-        <button class="msg btnAcerto">Reiniciar o jogo</button>
-        `;
-        const btnReiniciarJogo = document.querySelector('.btnAcerto');
-        btnReiniciarJogo.addEventListener('click', () => location.reload());
-        msgErro.classList.add('acerto');
-        msgErro.style.display = "block";
-        msgErro.innerHTML = `Parabéns! Você acertou!`;
-        for (let i = 0; i < palavraEscrita.length; i++) {
+  }
+  
+  const buscarPalavra = palavras.find(palavra => palavra.palavra.toUpperCase() == palavraEscrita);
+  const palavraExiste = buscarPalavra ? true : false;
+  
+  if (todosEspacosPreenchidos && palavraExiste) {
+    letras[numLinha].linhaPreenchida = true;
+    if (palavraEscrita != palavra) {
+      for (let i = 0; i < palavraEscrita.length; i++) {
+        if (palavraEscrita[i] == palavra[i]) {
           document.querySelector(`.letra${palavraEscrita[i]}`).style.backgroundColor = "#538D4E";
           document.querySelector(`.linha__wordle-${encontrarLinhaNaoPreenchida()}`).querySelector(`.letra${i + 1}`).style.backgroundColor = "#538D4E";
+        } else if (palavraEscrita[i] != palavra[i] && palavra.includes(palavraEscrita[i])) {
+          document.querySelector(`.letra${palavraEscrita[i]}`).style.backgroundColor = "#B59F3B";
+          document.querySelector(`.linha__wordle-${encontrarLinhaNaoPreenchida()}`).querySelector(`.letra${i + 1}`).style.backgroundColor = "#B59F3B";
+        } else {
+          document.querySelector(`.letra${palavraEscrita[i]}`).style.backgroundColor = "#585858";
         }
       }
-    } else if (!todosEspacosPreenchidos) {
-      msgErro.classList.add('erro-qtd-letras');
+      if (encontrarLinhaNaoPreenchida() == 6) {
+        msgErro.classList.add('fim-de-jogo');
+        msgErro.style.display = "block";
+        msgErro.innerHTML = `Você perdeu o jogo!`;
+      }
+    } else {
+      document.querySelector('.principal').innerHTML += `
+      <button class="msg btnAcerto">Reiniciar o jogo</button>
+      `;
+      const btnReiniciarJogo = document.querySelector('.btnAcerto');
+      btnReiniciarJogo.addEventListener('click', () => location.reload());
+      msgErro.classList.add('acerto');
       msgErro.style.display = "block";
-      msgErro.innerHTML = `Preencha todas as letras`;
-    } else if (!palavraExiste) {
-      msgErro.classList.add('erro-palavra-inexistente');
-      msgErro.style.display = "block";
-      msgErro.innerHTML = `Palavra não está na lista`;
+      msgErro.innerHTML = `Parabéns! Você acertou!`;
+      for (let i = 0; i < palavraEscrita.length; i++) {
+        document.querySelector(`.letra${palavraEscrita[i]}`).style.backgroundColor = "#538D4E";
+        document.querySelector(`.linha__wordle-${encontrarLinhaNaoPreenchida()}`).querySelector(`.letra${i + 1}`).style.backgroundColor = "#538D4E";
+      }
     }
+    console.log(contJogadas);
+    contJogadas += 1;
+    console.log(contJogadas);
+  } else if (!todosEspacosPreenchidos) {
+    msgErro.classList.add('erro-qtd-letras');
+    msgErro.style.display = "block";
+    msgErro.innerHTML = `Preencha todas as letras`;
+  } else if (!palavraExiste) {
+    msgErro.classList.add('erro-palavra-inexistente');
+    msgErro.style.display = "block";
+    msgErro.innerHTML = `Palavra não está na lista`;
+  }
+  
 }
 
 
@@ -306,24 +315,24 @@ function limparAviso() {
 }
 
 function inserirLetra(evento) {
-    limparAviso();
-    const local = encontrarUltimaPosicaoPreenchida();
-    const linha = encontrarLinhaNaoPreenchida();
-    const coluna = encontrarColunaNaoPreenchida();
-    let validarInsercao = false;
-  
-    if (coluna <= 4 && !letras.linhaPreenchida) {
-      letras[linha].colunas[coluna] = evento;
-      local.textContent = evento;
-  
-      validarInsercao = true;
-    } else {
-      msgErro.classList.add('erro-qtd-letras');
-      msgErro.style.display = "block";
-      msgErro.innerHTML = `Só pode inserir 5 letras`;
-    }
-  
-    return validarInsercao;
+  limparAviso();
+  const local = encontrarUltimaPosicaoPreenchida();
+  const linha = encontrarLinhaNaoPreenchida();
+  const coluna = encontrarColunaNaoPreenchida();
+  let validarInsercao = false;
+
+  if (coluna <= 4 && !letras.linhaPreenchida) {
+    letras[linha].colunas[coluna] = evento;
+    local.textContent = evento;
+
+    validarInsercao = true;
+  } else {
+    msgErro.classList.add('erro-qtd-letras');
+    msgErro.style.display = "block";
+    msgErro.innerHTML = `Só pode inserir 5 letras`;
+  }
+
+  return validarInsercao;
 }
 
 function encontrarUltimaPosicaoPreenchida() {
@@ -345,29 +354,29 @@ function encontrarUltimaPosicaoPreenchida() {
 }
 
 function encontrarLinhaNaoPreenchida() {
-    let linha = 0;
-    while (linha < letras.length) {
-      let elemento = letras[linha];
-      if (!elemento.linhaPreenchida) {
-        break;
-      }
-      linha++;
+  let linha = 0;
+  while (linha < letras.length) {
+    let elemento = letras[linha];
+    if (!elemento.linhaPreenchida) {
+      break;
     }
+    linha++;
+  }
 
-    return linha;
+  return linha;
 }
 
 function encontrarColunaNaoPreenchida() {
-    let linha = letras[encontrarLinhaNaoPreenchida()];
-    let coluna = 0;
+  let linha = letras[encontrarLinhaNaoPreenchida()];
+  let coluna = 0;
 
-    while (coluna < 5) {
-      let elemento = linha.colunas[coluna];
-      if (!elemento) {
-        break;
-      }
-      coluna++;
+  while (coluna < 5) {
+    let elemento = linha.colunas[coluna];
+    if (!elemento) {
+      break;
     }
+    coluna++;
+  }
 
-    return coluna;
+  return coluna;
 }
